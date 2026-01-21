291ee93f48fe7eaab51f683b7c72cba2c805b00b
hussein34535 <hussein34535@proton.me>
2026-01-21 03:11:59 +0200

    ب

--- install_flutter.sh
+++ install_flutter.sh
@@ -3,8 +3,10 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Clone Flutter SDK (Stable Channel)
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
+# Clone Flutter SDK (Stable Channel) if not exists
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
 
 # Add Flutter to PATH for the current session
 export PATH="$PATH:$(pwd)/flutter/bin"

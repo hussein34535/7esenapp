66a831da5944b40322386496edfbabdc45531e2d
hussein34535 <hussein34535@proton.me>
2026-02-09 01:35:53 +0200

    قق

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -1,20 +1,17 @@
-import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CloudinaryService {
   static const String _cloudName = 'djxkwged9';
   static const String _apiKey = '995382833695689';
   static const String _apiSecret = 'Y4zFQM1fFjBpNjSv7PpswVMhA8Q';
 
-  static Future<String> uploadImage(File imageFile) async {
+  static Future<String> uploadImage(XFile imageFile) async {
     final timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
 
     // 1. Generate Signature
-    // Signature = SHA1(params + secret)
-    // Params must be sorted alphabetically
-    // We are only sending 'timestamp' as a required signed param for standard upload
     final paramsToSign = 'timestamp=$timestamp$_apiSecret';
     final signature = sha1.convert(utf8.encode(paramsToSign)).toString();
 
@@ -27,9 +24,13 @@ class CloudinaryService {
     request.fields['timestamp'] = timestamp.toString();
     request.fields['signature'] = signature;
 
-    // Add the file
-    request.files
-        .add(await http.MultipartFile.fromPath('file', imageFile.path));
+    // Add the file (Web-compatible: use bytes)
+    final bytes = await imageFile.readAsBytes();
+    request.files.add(http.MultipartFile.fromBytes(
+      'file',
+      bytes,
+      filename: imageFile.name,
+    ));
 
     // 3. Send
     final response = await request.send();
